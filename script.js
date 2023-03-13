4f1bf49e06327e00ea063e94af3a0a76db86d52d
chain <klaus.mika1052@gmail.com>
2023-03-13 16:06:20 -0300

    adicionado scroll suave na navegação

--- script.js
+++ script.js
@@ -37,3 +37,22 @@
     });
   }
 })();
+
+(() => {
+  const links = document.querySelectorAll('.js-menu a[href=^"#"]');
+
+  const scrollToSection = (event) => {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  links.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+})();
