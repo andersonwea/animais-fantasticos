d7382f084552ae432d9080b6c8f17da6a081a75f
chain <klaus.mika1052@gmail.com>
2023-03-22 14:29:01 -0300

    adicionado função de abir e fechar menu mobile e animaçoes

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,3 +1,21 @@
-const initMenuMobile = () => {};
+import outsideClick from "./outsideclick.js";
 
+const initMenuMobile = () => {};
+const menuMobile = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+const events = ["click", "touchstart"];
+
+function handleClick() {
+  menuMobile.classList.add("ativo");
+  menuList.classList.add("ativo");
+
+  outsideClick(menuList, events, () => {
+    menuList.classList.remove("ativo");
+    menuMobile.classList.remove("ativo");
+  });
+}
+
+events.forEach((event) => {
+  menuMobile.addEventListener(event, handleClick);
+});
 export default initMenuMobile;
--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -2,6 +2,15 @@ const outsideClick = (element, events, callback) => {
   const html = document.documentElement;
   const outside = "data-outside";
 
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      }, 100);
+    });
+    element.setAttribute(outside, "");
+  }
+
   const handleOutsideClick = (event) => {
     if (!element.contains(event.target)) {
       element.removeAttribute(outside);
@@ -11,13 +20,6 @@ const outsideClick = (element, events, callback) => {
       callback();
     }
   };
-
-  if (!element.hasAttribute(outside)) {
-    events.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    });
-    element.setAttribute(outside, "");
-  }
 };
 
 export default outsideClick;
