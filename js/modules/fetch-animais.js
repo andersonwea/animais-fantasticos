f60da66f11dbc6a4c94f25259d829725aed57bd4
chain <klaus.mika1052@gmail.com>
2023-03-31 15:46:40 -0300

    refatorado fetch-animais

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,6 +1,10 @@
 import AnimaNumeros from "./anima-numeros.js";
 
-const initFetchAnimais = () => {
+const fetchAnimais = (url, target) => {
+  const numerosGrid = document.querySelector(target);
+
+  // Cria a div contendo informações
+  // com o total de animais
   const createAnimal = (animal) => {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
@@ -8,28 +12,36 @@ const initFetchAnimais = () => {
     return div;
   };
 
-  const fetchAnimais = async (url) => {
+  // Preenche cada animal no Dom
+  const preencherAnimais = (animal) => {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  };
+
+  // Anima os números de cada animal
+  const animaAnimaisNumeros = () => {
+    const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", "ativo");
+    animaNumeros.init();
+  };
+
+  // Puxa os animais através de um arquivo json
+  // e cria cada animal utilizando createAnimal
+  const criarAnimais = async () => {
     try {
-      const numerosGrid = document.querySelector(".numeros-grid");
+      //  Fetch e espera a resposta e transforma a resposta em json
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
 
-      animaisJSON.forEach((element) => {
-        const divAnimal = createAnimal(element);
-        numerosGrid.appendChild(divAnimal);
-      });
-      const animaNumeros = new AnimaNumeros(
-        "[data-numero]",
-        ".numeros",
-        "ativo"
-      );
-      animaNumeros.init();
+      // Após a transformação de json, ativa as funções
+      // para preencher e animar os números
+      animaisJSON.forEach((animal) => preencherAnimais(animal));
+      animaAnimaisNumeros();
     } catch (error) {
       console.log(Error(error));
     }
   };
 
-  fetchAnimais("./animaisapi.json");
+  return criarAnimais();
 };
 
-export default initFetchAnimais;
+export default fetchAnimais;
