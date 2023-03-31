d9187ccc556636aa48f91973c75c69b29305a6ea
chain <klaus.mika1052@gmail.com>
2023-03-31 16:15:36 -0300

    fetch-bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,12 +1,13 @@
-const initBitcoin = () => {};
-const btc = document.querySelector(".btc");
+const fetchBitcoin = (url, target) => {
+  const btc = document.querySelector(target);
 
-fetch("https://blockchain.info/ticker")
-  .then((response) => response.json())
-  .then((responseJSON) => {
-    btc.innerText = (1000 / responseJSON.BRL.buy).toFixed(4);
-  })
-  .catch((error) => {
-    console.log(Error(error));
-  });
-export default initBitcoin;
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      btc.innerText = (1000 / responseJSON.BRL.buy).toFixed(4);
+    })
+    .catch((error) => {
+      console.log(Error(error));
+    });
+};
+export default fetchBitcoin;
